86b7fe76a19adcf36e63818d8b43b2bdda36f134
Joahn Aml <joahn_aml@outlook.com>
2023-05-10 16:53:02 +0200

    feat: spring cuboid scale on hover

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -1,5 +1,10 @@
 <script>
 	import { T } from '@threlte/core';
+	import { interactivity } from '@threlte/extras';
+	import { spring } from 'svelte/motion';
+
+	interactivity();
+	const scale = spring(1);
 </script>
 
 <T.PerspectiveCamera
@@ -10,7 +15,12 @@
 	}}
 />
 
-<T.Mesh position.y={1}>
+<T.Mesh
+	position.y={1}
+	scale={$scale}
+	on:pointerenter={() => scale.set(1.5)}
+	on:pointerleave={() => scale.set(1)}
+>
 	<T.BoxGeometry args={[1, 2, 1]} />
 	<T.MeshBasicMaterial color="hotpink" />
 </T.Mesh>
