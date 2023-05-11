a251ee8c293e386a1e58e9f6f5e10715882cfa67
Joahn Aml <joahn_aml@outlook.com>
2023-05-11 09:26:47 +0200

    feat: add camera controls

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -1,14 +1,19 @@
 <script>
 	import { T, useFrame } from '@threlte/core';
-	import { interactivity } from '@threlte/extras';
+	import { interactivity, OrbitControls } from '@threlte/extras';
 	import { spring } from 'svelte/motion';
+	import { get } from 'svelte/store';
 
 	interactivity();
 	const scale = spring(1);
 
 	let rotation = 0;
-	useFrame((state, delta) => {
-		rotation += delta;
+	const rotationSpeed = spring(0, {
+		stiffness: 0.01,
+		damping: 1
+	});
+	useFrame((_state, delta) => {
+		rotation += get(rotationSpeed) * delta;
 	});
 </script>
 
@@ -18,16 +23,23 @@
 	on:create={({ ref }) => {
 		ref.lookAt(0, 1, 0);
 	}}
-/>
+>
+	<OrbitControls enableDamping />
+</T.PerspectiveCamera>
 
 <T.DirectionalLight position={[3, 10, 7]} />
+<T.AmbientLight intensity={0.1} />
 
 <T.Mesh
 	rotation.y={rotation}
 	position.y={1}
 	scale={$scale}
-	on:pointerenter={() => scale.set(1.5)}
+	on:pointerenter={() => {
+		scale.set(1.1);
+		rotationSpeed.set(0);
+	}}
 	on:pointerleave={() => scale.set(1)}
+	on:click={() => rotationSpeed.set(1)}
 >
 	<T.BoxGeometry args={[1, 2, 1]} />
 	<T.MeshStandardMaterial color="aquamarine" />
