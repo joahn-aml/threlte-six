d2b845140f476b3ebb8d010c07bd5a61413abc65
Joahn Aml <joahn_aml@outlook.com>
2023-05-13 01:31:39 +0200

    refactor: user dynamic canvas size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
 <script>
 	import { Canvas } from '@threlte/core';
 	import Scene from '$lib/Scene.svelte';
+
+	let width = window.innerWidth;
+	let height = window.innerHeight;
 </script>
 
-<Canvas><Scene /></Canvas>
+<svelte:window
+	on:resize={() => {
+		width = window.innerWidth;
+		height = window.innerHeight;
+	}}
+/>
+
+<Canvas size={{ width, height }}><Scene /></Canvas>
